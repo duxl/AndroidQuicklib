0ee9d09915da3fc2d454194cf07fd7aeea5df955
duxl <645575165@qq.com>
2024-01-03 13:20:02 +0800

    新增自定义控件-密码输入框

---
+++ app/src/main/java/com/duxl/android/quicklib/TestPwdEditActivity.java
@@ -0,0 +1,14 @@
+package com.duxl.android.quicklib;
+
+import com.duxl.baselib.ui.activity.BaseActivity;
+
+/**
+ * 测试密码输入框
+ */
+public class TestPwdEditActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_test_pwd_edit;
+    }
+}
