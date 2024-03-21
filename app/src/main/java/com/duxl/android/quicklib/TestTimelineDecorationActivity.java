2a092d2add5d0fe1ff4f44396f278fdc2ee14e17
duxl <645575165@qq.com>
2024-03-21 10:15:18 +0800

    时间线装饰器扩展升级：增加完全自定义效果

--- app/src/main/java/com/duxl/android/quicklib/TestTimelineDecorationActivity.java
+++ app/src/main/java/com/duxl/android/quicklib/TestTimelineDecorationActivity.java
@@ -1,10 +1,13 @@
 package com.duxl.android.quicklib;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.duxl.android.quicklib.adapter.SimpleAdapter;
@@ -99,13 +102,70 @@ public class TestTimelineDecorationActivity extends BaseActivity {
 
             @Override
             public Drawable getDot(int position) {
-                if(position == 0) {
+                if (position == 0) {
                     return getResources().getDrawable(R.mipmap.ic_time);
                 }
                 return super.getDot(position);
             }
 
         };
+        mRecyclerView.addItemDecoration(timelineDecoration);
+    }
+
+    // 完全自定义
+    public void onClickCustomAll(View v) {
+        removeAll();
+        int outRight = DisplayUtil.dip2px(this, 40);
+        TimelineDecoration timelineDecoration = new TimelineDecoration(this, 0, new TimelineDecoration.DrawCallback() {
+            @Override
+            public void drawItem(@NonNull Canvas canvas, @NonNull Paint paint, @NonNull RecyclerView parent, @NonNull View child, int position) {
+                paint.reset();
+                paint.setAntiAlias(true);
+                if (position % 2 == 0) {
+                    paint.setColor(Color.RED);
+                } else {
+                    paint.setColor(Color.GREEN);
+                }
+                int lineWidth = DisplayUtil.dip2px(TestTimelineDecorationActivity.this, 4);
+                // 画线条
+                paint.setStyle(Paint.Style.FILL);
+                paint.setStrokeWidth(lineWidth);
+                int startX = child.getRight() + outRight / 2;
+                int startY = child.getTop();
+                int endX = startX;
+                int endY = child.getBottom();
+
+                if (position == 0) {
+                    startY += child.getHeight() / 2;
+                } else if (position == parent.getAdapter().getItemCount() - 1) {
+                    endY -= child.getHeight() / 2;
+                }
+                canvas.drawLine(startX, startY, endX, endY, paint);
+
+                // 画圆点
+                paint.setColor(Color.parseColor("#440000FF"));
+                int radius = DisplayUtil.dip2px(TestTimelineDecorationActivity.this, 10);
+                int centerY = child.getTop() + child.getHeight() / 2;
+                canvas.drawCircle(startX, centerY, radius, paint);
+
+                // 画编号
+                int textSize = DisplayUtil.sp2px(TestTimelineDecorationActivity.this, 14);
+                paint.setTextSize(textSize);
+                paint.setAlpha(255);
+                paint.setColor(Color.BLUE);
+                paint.setTextAlign(Paint.Align.CENTER);
+                Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+                int baseLine = (int)(child.getTop() - fontMetrics.top + (child.getHeight() - (fontMetrics.bottom - fontMetrics.top)) / 2);
+                canvas.drawText(String.valueOf(position), startX, baseLine, paint);
+            }
+        }) {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                //super.getItemOffsets(outRect, view, parent, state);
+                outRect.right = outRight;
+            }
+        };
+
         mRecyclerView.addItemDecoration(timelineDecoration);
     }
 }
