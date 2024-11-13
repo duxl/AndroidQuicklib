3e2b87cf488631284b568ce58418194e691071e9
duxl <645575165@qq.com>
2024-11-13 11:17:12 +0800

    增减监听软件是否显示

--- app/src/main/java/com/duxl/android/quicklib/TestInputActivity.java
+++ app/src/main/java/com/duxl/android/quicklib/TestInputActivity.java
@@ -1,10 +1,13 @@
 package com.duxl.android.quicklib;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.duxl.baselib.ui.activity.BaseActivity;
+import com.duxl.baselib.utils.DisplayUtil;
 import com.duxl.baselib.utils.WindowSoftInputCompat;
+import com.duxl.baselib.widget.DataRunnable;
 
 /**
  * 键盘遮挡输入框问题
@@ -24,5 +27,20 @@ public class TestInputActivity extends BaseActivity {
     protected void initView(View v) {
         ((TextView) v.findViewById(R.id.tv_top)).setText(msg);
         WindowSoftInputCompat.assist(this);
+
+        handKeyboardVisibleChanged();
+    }
+
+    /**
+     * 检测软键盘显示与隐藏
+     */
+    private void handKeyboardVisibleChanged() {
+        View vTop = findViewById(R.id.tv_top);
+        vTop.setOnClickListener(v -> {
+            Log.i("duxl.log", "软键盘是否显示方法1: " + DisplayUtil.isKeyboardShow(TestInputActivity.this));
+            Log.i("duxl.log", "软键盘是否显示方法2: " + DisplayUtil.isKeyboardAcceptingText(TestInputActivity.this));
+            Log.i("duxl.log", "软键盘是否作用与第一个输入框: " + DisplayUtil.isKeyboardShow(TestInputActivity.this, findViewById(R.id.edit01)));
+        });
+        DisplayUtil.observerKeyboardVisibleChanged(vTop, isVisible -> Log.i("duxl.log", "监听到软键盘显示或隐藏: isVisible=" + isVisible));
     }
 }
