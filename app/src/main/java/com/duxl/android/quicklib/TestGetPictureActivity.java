29ce4f42c2429c2596b37eeb5829f859928c3663
duxl <645575165@qq.com>
2025-03-26 11:17:26 +0800

    移除rxpermissions

--- app/src/main/java/com/duxl/android/quicklib/TestGetPictureActivity.java
+++ app/src/main/java/com/duxl/android/quicklib/TestGetPictureActivity.java
@@ -8,24 +8,19 @@ import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
 import com.duxl.android.quicklib.databinding.ActivityTestGetPictureBinding;
-import com.duxl.baselib.rx.SimpleObserver;
-import com.duxl.baselib.ui.activity.BaseActivity;
+import com.duxl.baselib.ui.activity.SupportComponentResultActivity;
 import com.duxl.baselib.utils.AlbumUtils;
 import com.duxl.baselib.utils.EmptyUtils;
 import com.duxl.baselib.utils.ToastUtils;
-import com.tbruyelle.rxpermissions3.Permission;
-import com.tbruyelle.rxpermissions3.RxPermissions;
 import com.zhihu.matisse.Matisse;
 
 import java.util.List;
-
-import io.reactivex.rxjava3.annotations.NonNull;
 
 /**
  * rxpermissions
  * create by duxl 2020/8/19
  */
-public class TestGetPictureActivity extends BaseActivity {
+public class TestGetPictureActivity extends SupportComponentResultActivity {
 
     private ActivityTestGetPictureBinding mBinding;
 
@@ -53,50 +48,22 @@ public class TestGetPictureActivity extends BaseActivity {
     }
 
     private void permissions() {
-        RxPermissions rxPermissions = new RxPermissions(this);
-        // 1、使用request
-//        rxPermissions
-//                .request(Manifest.permission.CAMERA)
-//                .subscribe(new SimpleObserver<Boolean>() {
-//                    @Override
-//                    public void onNext(@NonNull Boolean aBoolean) {
-//                        ToastUtils.show("权限：" + aBoolean);
-//                    }
-//                });
-
-
-        // 2、使用requestEach
-//        rxPermissions
-//                .requestEach(Manifest.permission.CAMERA, Manifest.permission.READ_PHONE_STATE)
-//                .subscribe(new SimpleObserver<Permission>() {
-//                    @Override
-//                    public void onNext(@NonNull Permission permission) {
-//                        if(permission.granted) {
-//                            ToastUtils.show("有"+permission.name+"权限");
-//                        } else if(permission.shouldShowRequestPermissionRationale) {
-//                            ToastUtils.show("需要重写申请权限");
-//                        } else {
-//                            ToastUtils.show("获取权限失败");
-//                        }
-//                    }
-//                });
-
-        // 3、使用requestEachCombined
-        rxPermissions
-                .requestEachCombined(Manifest.permission.CAMERA, Manifest.permission.READ_PHONE_STATE)
-                .subscribe(new SimpleObserver<Permission>() {
-                    @Override
-                    public void onNext(@NonNull Permission permission) {
-                        if (permission.granted) {
-                            ToastUtils.show("有" + permission.name + "权限");
-                            System.out.println("duxl.debug:: " + "有" + permission.name + "权限");
-                        } else if (permission.shouldShowRequestPermissionRationale) {
-                            ToastUtils.show("需要重写申请权限");
+        launchRequestPermissions(
+                new String[]{
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_PHONE_STATE
+                }, resultMap -> {
+                    for (String permission : resultMap.keySet()) {
+                        boolean granted = Boolean.TRUE.equals(resultMap.get(permission));
+                        if (granted) {
+                            ToastUtils.show("有" + permission + "权限");
+                            System.out.println("duxl.debug:: " + "有" + permission + "权限");
                         } else {
-                            ToastUtils.show("获取权限失败");
+                            ToastUtils.show("获取" + permission + "权限失败");
                         }
                     }
-                });
+                }
+        );
     }
 
     @Override
