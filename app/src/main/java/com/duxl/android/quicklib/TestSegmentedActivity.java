f6a8e262e432940e41442bc1bec8dcbb09d93452
duxl <645575165@qq.com>
2025-10-24 16:27:37 +0800

    SegmentedControlView滑块支持设置滑动偏移量（可实现与ViewPager滑动同步效果）

--- app/src/main/java/com/duxl/android/quicklib/TestSegmentedActivity.java
+++ app/src/main/java/com/duxl/android/quicklib/TestSegmentedActivity.java
@@ -1,10 +1,14 @@
 package com.duxl.android.quicklib;
 
+import static androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_DRAGGING;
+
 import android.view.View;
 
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.duxl.android.quicklib.adapter.SimpleAdapter;
 import com.duxl.android.quicklib.databinding.ActivityTestSegmentedBinding;
 import com.duxl.baselib.ui.activity.BaseActivity;
-import com.duxl.baselib.utils.ToastUtils;
 import com.duxl.baselib.widget.segmented.SegmentedControlItem;
 
 /**
@@ -13,6 +17,7 @@ import com.duxl.baselib.widget.segmented.SegmentedControlItem;
 public class TestSegmentedActivity extends BaseActivity {
 
     private ActivityTestSegmentedBinding binding;
+    private boolean isDragging; // 是否正在拖拽ViewPager
 
     @Override
     protected int getLayoutResId() {
@@ -27,7 +32,33 @@ public class TestSegmentedActivity extends BaseActivity {
         binding.scv.addItem(new SegmentedControlItem("Java"));
         binding.scv.addItem(new SegmentedControlItem("Kotlin"));
         binding.scv.addItem(new SegmentedControlItem("Android"));
+        binding.scv.setOnSegItemClickListener((item, position) -> {
+            binding.viewPager.setCurrentItem(position);
+        });
 
-        binding.scv.setOnSegItemClickListener((item, position) -> ToastUtils.show("onItemClick#" + position));
+
+        binding.viewPager.setAdapter(new SimpleAdapter(3, true));
+        binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (isDragging) {
+                    if (position == binding.viewPager.getCurrentItem()) {
+                        binding.scv.setItemOffset(positionOffset);
+                    } else {
+                        binding.scv.setItemOffset(positionOffset - 1);
+                    }
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                binding.scv.setSelectedItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                isDragging = state == SCROLL_STATE_DRAGGING;
+            }
+        });
     }
 }
--- app/src/main/java/com/duxl/android/quicklib/adapter/SimpleAdapter.java
+++ app/src/main/java/com/duxl/android/quicklib/adapter/SimpleAdapter.java
@@ -1,6 +1,8 @@
 package com.duxl.android.quicklib.adapter;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,24 +14,41 @@ import androidx.recyclerview.widget.RecyclerView;
 public class SimpleAdapter extends RecyclerView.Adapter {
 
     private int mItemCount;
+    private boolean isFullScreen;
+    private int[] colors = {Color.RED, Color.BLUE, Color.BLACK, Color.YELLOW};
 
     public SimpleAdapter(int count) {
         this.mItemCount = count;
+    }
+
+    public SimpleAdapter(int count, boolean isFullScreen) {
+        this(count);
+        this.isFullScreen = isFullScreen;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        if (isFullScreen) {
+            view.setLayoutParams(new RecyclerView.LayoutParams(
+                    RecyclerView.LayoutParams.MATCH_PARENT,
+                    RecyclerView.LayoutParams.MATCH_PARENT
+            ));
+            ((TextView) view).setGravity(Gravity.CENTER);
+            ((TextView) view).setTextSize(30);
+        }
         return new RecyclerView.ViewHolder(view) {
         };
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         TextView tv = holder.itemView.findViewById(android.R.id.text1);
-        tv.setText("Position" + position);
-        tv.setBackgroundColor(Color.parseColor(position % 2 == 0 ? "#EEEEEE" : "#DDDDDD"));
+        tv.setText("Position# " + position);
+        tv.setBackgroundColor(colors[position % colors.length]);
+        tv.setTextColor(Color.WHITE);
     }
 
     @Override
